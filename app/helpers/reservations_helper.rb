0a83c5c608dff2a488f52cfedb82b0fc964bfba7
amalrik <amalrik.maia@gmail.com>
2015-05-10 23:08:06 -0300

    added sync gem

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,7 +1,5 @@
 module ReservationsHelper
   def make_link_for_slots(slot, day, hour)
-    #byebug
-    #link_to "Remove", update_products_path(product:{parent_id: nil}), method: :put
     link_to_unless  slot.position["[#{hour.to_i}, :#{day}]"], 
                     "Reservar sala", 
                     reservation_path(slot, reservation: { hour: hour, day: day.to_s}), :method => :put do
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,11 +2,11 @@ class Reservation < ActiveRecord::Base
   belongs_to :user
   store_accessor :position
 
-  def self.book_on_last(hour, day, user)
+  def self.book_on_last(hour, day)
     transaction do
       reservation = Reservation.last
       return false if reservation.position["[#{hour}, :#{day}]"]
-      reservation.position[[hour, day]] = user.id
+      reservation.position[[hour, day]] = "booked"
       reservation.save
     end
   end
